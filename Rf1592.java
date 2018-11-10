e5e3ef54503d9a8efe22cc1b566c86ee163740fd
Gotaro AMENOMORi <gotaroamenomori@gmail.com>
2018-11-10 17:18:19 +0900

    kaijoメソッド追加

--- Rf1592.java
+++ Rf1592.java
@@ -1,5 +1,16 @@
 public class Rf1592{
     public static void main(String[] ags){
 	System.out.println("Hello World");
+	if(ags.length > 0){
+	    System.out.println(kaijo(Integer.parseInt(ags[0])));
+	}
+    }
+
+    public static int kaijo(int num){
+	int ret=1;
+	for(int i=0; i<num; i++){
+	    ret = ret * (i+1);
+	}
+	return ret;
     }
 }
